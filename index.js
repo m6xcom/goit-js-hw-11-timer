4dcfaa69a4092b6bc9ee2c7bb8d6077373104851
m6xcom <65496114+m6xcom@users.noreply.github.com>
2020-12-12 19:59:59 +0200

    Устанавливает первонажальное значение

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const hoursSpan = document.querySelector("[data-value='hours']");
 const minsSpan = document.querySelector("[data-value='mins']");
 const secsSpan = document.querySelector("[data-value='secs']");
 
-targetDate = new Date("Jan 1, 2021");
+let targetDate = new Date("Jan 1, 2021");
 
 class CountdownTimer {
   constructor(date) {
     this.targetDate = date;
+    this.setCountdown();
   }
-  updateTimer = setInterval(() => {
+  setCountdown() {
     let time = this.targetDate - Date.now();
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -19,6 +20,9 @@ class CountdownTimer {
     hoursSpan.textContent = String(hours).padStart(2, 0);
     minsSpan.textContent = String(mins).padStart(2, 0);
     secsSpan.textContent = String(secs).padStart(2, 0);
+  }
+  updateTimer = setInterval(() => {
+    this.setCountdown();
   }, 1000);
 }
 
