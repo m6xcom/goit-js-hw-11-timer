7d02ad9b477943ae1899e429b90ec6fc606cd539
m6xcom <65496114+m6xcom@users.noreply.github.com>
2020-12-12 18:00:49 +0200

    Добавляет код таймера

---
+++ index.js
@@ -0,0 +1,25 @@
+const daysSpan = document.querySelector("[data-value='days']");
+const hoursSpan = document.querySelector("[data-value='hours']");
+const minsSpan = document.querySelector("[data-value='mins']");
+const secsSpan = document.querySelector("[data-value='secs']");
+
+targetDate = new Date("Jan 1, 2021");
+
+class CountdownTimer {
+  constructor(date) {
+    this.targetDate = date;
+  }
+  updateTimer = setInterval(() => {
+    let time = this.targetDate - Date.now();
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    daysSpan.textContent = String(days).padStart(2, 0);
+    hoursSpan.textContent = String(hours).padStart(2, 0);
+    minsSpan.textContent = String(mins).padStart(2, 0);
+    secsSpan.textContent = String(secs).padStart(2, 0);
+  }, 1000);
+}
+
+new CountdownTimer(targetDate);
